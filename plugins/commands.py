5d0ab64c5f3b74678b4ccfab08be878d0597dd29
Brokenboy204 <111448245+Brokenboy204@users.noreply.github.com>
2022-08-26 14:15:22 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -41,13 +41,13 @@ async def start(client, message):
         buttons = [[
             InlineKeyboardButton('⚚ ΛᎠᎠ MΞ ϮԾ YԾUᏒ GᏒԾUᎮ ⚚', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
             ],[
-            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/MWUpdatez'),
-            InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/OpusTechz')
+            InlineKeyboardButton('💠 CHΛИИΞL 💠', url='https://t.me/Broken_Movies_Request_Group'),
+            InlineKeyboardButton('💠 UᎮDΛTΞS 💠', url='https://t.me/Broken_movies204')
             ],[      
             InlineKeyboardButton('♻️ HΞLᎮ ♻️', callback_data='help'),
             InlineKeyboardButton('♻️ ΛBOUT ♻️', callback_data='about')
             ],[
-            InlineKeyboardButton('✅ SUBSCᏒIBΞ  ✅', url='https://youtube.com/channel/UCf_dVNrilcT0V2R--HbYpMA')
+            InlineKeyboardButton('✅ Request Your Movies  ✅', url='https://t.me/+YZRWmAFSGvA1ODVl')
         ]]         
         reply_markup = InlineKeyboardMarkup(buttons)        
         await message.reply_photo(
